d6c4397d257aed0bc86c5faf717dc79f49f9977a
oopsEthan <ethan_here@proton.me>
2024-09-13 11:25:18 -0500

    Created initial files, grass grower moment

---
+++ main.py
@@ -0,0 +1,9 @@
+from turtle import *
+
+# goals
+# - Encapsulate everything, need classes for:
+#   - Both paddles, one controlled by player, other just goes up and down
+#   - Line down the middle, possibly unnecessary but should be easy to just draw
+#   - Score at top, big numbers
+#   - Ball, bounces on bottom or top of screen and off paddle
+#   - Game runs in main, probably a class just for "screen set up"
